2a6a5ae30bbeea17fe1155a269c20d96367a4125
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-15 16:57:52 +0800

    Create any.js

---
+++ src/types/extra/any.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-restricted-syntax */
+import TypeCheckInterface from '../typecheck-interface';
+
+/**
+ * @ignore
+ */
+class Any extends TypeCheckInterface {
+  is(value) {
+    return true;
+  }
+
+  toString() {
+    return 'Any';
+  }
+}
+
+export default new Any();
