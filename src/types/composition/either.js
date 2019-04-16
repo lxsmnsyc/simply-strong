d46e62e69c5918abf0ebb49a54e2bcd7bffa695f
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-16 18:00:17 +0800

    Update either.js

--- src/types/composition/either.js
+++ src/types/composition/either.js
@@ -1,26 +1,25 @@
 /* eslint-disable no-restricted-syntax */
 import TypeCheckInterface from '../typecheck-interface';
+import { checkArgumentType } from '../utils';
 
 class Either extends TypeCheckInterface {
-  constructor(...types) {
+  constructor(...T) {
     super();
     let i = 0;
-    for (const type of types) {
+    for (const type of T) {
       i += 1;
-      if (!(type instanceof TypeCheckInterface)) {
-        throw new TypeError(`Expected argument #${i} to be a TypeCheckInterface instance.`);
-      }
+      checkArgumentType(type, TypeCheckInterface, i);
     }
 
-    this.types = types;
+    this.T = T;
 
-    const typeTag = types.reduce((acc, x) => `${acc}, ${x}`);
+    const typeTag = T.reduce((acc, x) => `${acc}, ${x}`);
     this.typeTag = `Either(${typeTag})`;
   }
 
   is(value) {
     // eslint-disable-next-line valid-typeof
-    for (const type of this.types) {
+    for (const type of this.T) {
       if (type.is(value)) {
         return true;
       }
@@ -33,4 +32,4 @@ class Either extends TypeCheckInterface {
   }
 }
 
-export default (...types) => new Either(...types);
+export default (...T) => new Either(...T);
