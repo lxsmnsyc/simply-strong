9b5dd3b142dc2a1f5b47dc699bba7f619de20ba9
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-25 15:11:58 +0800

    Create class.js

---
+++ src/types/class/class.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-restricted-syntax */
+import TypeCheckInterface from '../typecheck-interface';
+import { isObject } from '../utils';
+
+class Class extends TypeCheckInterface {
+  constructor(C) {
+    super();
+    this.C = C;
+  }
+
+  equals(other) {
+    return other instanceof Class && this.C === other.C;
+  }
+
+  is(value) {
+    return isObject(value)
+      && isObject(value.prototype)
+      && value instanceof this.C;
+  }
+
+  toString() {
+    return `<class ${this.C.name}>`;
+  }
+}
+
+export default C => new Class(C);
