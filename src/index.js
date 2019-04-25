324a27465029cd9fb5d0a7656e45c654cd5e8b00
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-25 08:51:10 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+export { default as Def } from './types/data-structures/def';
 export { default as TypedMap } from './types/data-structures/map';
 
 export { default as Compose } from './types/composition/compose';
@@ -6,11 +6,17 @@ export { default as Either } from './types/composition/either';
 
 export { default as Nullable } from './types/annotations/nullable';
 export { default as Class } from './types/annotations/class';
+export { default as Super } from './types/annotations/super';
+export { default as Extends } from './types/annotations/extends';
+export { default as Except } from './types/annotations/except';
 
 export { default as Iterable } from './types/extra/iterable';
 export { default as Thenable } from './types/extra/thenable';
 export { default as NonNull } from './types/extra/nonnull';
 export { default as AsyncIterable } from './types/extra/async-iterable';
 export { default as Any } from './types/extra/any';
+
+export { default as Like } from './types/objects/like';
+export { default as Exact } from './types/objects/exact';
 
 export { default as Types } from './types/primitives';
