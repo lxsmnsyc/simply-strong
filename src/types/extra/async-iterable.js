62c5f2c5b547ad49331ae8aa68c6751bbf41edc9
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-25 08:51:24 +0800

    Update async-iterable.js

--- src/types/extra/async-iterable.js
+++ src/types/extra/async-iterable.js
@@ -1,13 +1,14 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-restricted-syntax */
 import TypeCheckInterface from '../typecheck-interface';
+import { isFunction } from '../utils';
 
 /**
  * @ignore
  */
 class AsyncIterable extends TypeCheckInterface {
   is(value) {
-    return value != null && typeof value[Symbol.asyncIterator] === 'function';
+    return value != null && isFunction(value[Symbol.iterator]);
   }
 
   toString() {
