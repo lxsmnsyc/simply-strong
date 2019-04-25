55d73362024b4d7ff95609c247a28d561c7bc4ff
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-25 10:09:05 +0800

    Update def.js

--- src/types/data-structures/def.js
+++ src/types/data-structures/def.js
@@ -1,6 +1,9 @@
+/* eslint-disable func-names */
 /* eslint-disable no-restricted-syntax */
 import TypeCheckInterface from '../typecheck-interface';
-import { checkType } from '../utils';
+import {
+  checkType, isFunction, assert, zip,
+} from '../utils';
 
 class Def extends TypeCheckInterface {
   constructor(PTs, RT) {
@@ -16,6 +19,41 @@ class Def extends TypeCheckInterface {
     this.RT = RT;
 
     this.typeTag = `Def(${[...PTs, RT].reduce((acc, x) => `${acc} => ${x}`)})`;
+  }
+
+  create(fn) {
+    assert(isFunction(fn), 'Expected argument #1 to be a function.');
+
+    const { PTs, RT } = this;
+
+    const newFunction = function (...args) {
+      let counter = 0;
+      for (const [a, b] of zip(PTs, args)) {
+        counter += 1;
+        assert(a.is(b), `Expected argument #${counter} to be of type "${a}"`);
+      }
+
+      const result = fn.apply(this, args);
+      assert(RT.is(result), `Expected return type to be "${RT}"`);
+      return result;
+    };
+
+    newFunction.origin = this;
+
+    return newFunction;
+  }
+
+  equals(other) {
+    return other instanceof Def
+      && (this === other || (
+        this.RT.equals(other.RT)
+          && zip(this.PTs, other.PTs)
+            .reduce((acc, [a, b]) => acc && a.equals(b))
+      ));
+  }
+
+  is(value) {
+    return isFunction(value) && value.origin instanceof Def && this.equals(value.origin);
   }
 
   toString() {
