0923a187182f75e59292c6f7ff673821a34cd48a
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-25 08:51:33 +0800

    Update thenable.js

--- src/types/extra/thenable.js
+++ src/types/extra/thenable.js
@@ -1,6 +1,7 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-restricted-syntax */
 import TypeCheckInterface from '../typecheck-interface';
+import { isObject, isFunction } from '../utils';
 
 /**
  * @ignore
@@ -8,13 +9,12 @@ import TypeCheckInterface from '../typecheck-interface';
 class Thenable extends TypeCheckInterface {
   is(value) {
     return !!value
-      && (typeof value === 'object' || typeof value === 'function')
-      && typeof value.then === 'function';
+      && (isObject(value) || isFunction(value))
+      && isFunction(value.then);
   }
 
   toString() {
     return 'Thenable';
   }
 }
-
 export default new Thenable();
